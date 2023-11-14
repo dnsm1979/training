2088bd9ba795932c2fc4d4e52091cef2fd05df90
dnsm1979 <dnsm@rambler.ru>
2023-11-14 16:36:38 +0500

    final commit

---
+++ Newbranch/DZ_10.py
@@ -0,0 +1,18 @@
+# 1 задание
+# string = input('Введите стоку для проверки: ')
+# str_2 = string.replace(' ', '')
+# if str_2.lower() == str_2.lower()[::-1]:
+#     print('Строка является палиндромом!')
+# else:
+#     print('Строка не является палиндромом!')
+
+# 2 задание
+# string = input('Введите текст: ').split()
+# Words = list(map(str, input('Введите слова через пробел: ').split()))
+# sm = set(string)
+# print([a for a in sm if a in Words])
+
+string = input('Введите текст: ')
+Words = input('Введите слово: ')
+print(string.find(Words))
+
---
+++ Newbranch/DZ_8.py
@@ -0,0 +1,36 @@
+
+# # Первое задание
+# number_1 = int(input('Введите первое число: '))
+# number_2 = int(input('Введите второе число: '))
+# number_3 = int(input('Введите третье число: '))
+# print('Сумма = ', number_1 + number_2 + number_3)
+# print('Произведение = ', number_1 * number_2 * number_3)
+
+# Второе задание
+#
+# number_1 = int(input('Введите зарплату: '))
+# number_2 = int(input('Введите платеж по кредиту: '))
+# number_3 = int(input('Введите сумму камунальных платежей: '))
+# print('У вас на руках осталось: ', number_1 - number_2 - number_3)
+
+# Третье задание
+
+# diagonal_1 = int(input('Введите первую диаганаль: '))
+# diagonal_2 = int(input('Введите вторую диагональ: '))
+# print('Площадь ромба ровна: ', (diagonal_1 * diagonal_2) / 2 )
+
+Четвертое задание
+
+print('To be')
+print('or not')
+print('to be')
+
+
+
+# Или
+
+print('To be\nor not\nto be')
+
+# Пятое занятие
+
+# print('"Life is what happens\n\t\twhen\n\t\t\tyou re busy making other plans"\n\t\t\t\t\t\t\t\t\t\tJohn Lennon')
