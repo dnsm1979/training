554469ce1d0d3cdeedea4c38173b5e5fb94ab227
dnsm1979 <dnsm@rambler.ru>
2023-11-19 20:14:42 +0500

    Add tests DZ_28_2

--- DZ_28_2.py
+++ DZ_28_2.py
@@ -102,7 +102,21 @@ def test_product_creation_by_factory():
         fettuccine_alfredo, Fettuccine_Alfredo
     ), "ProductB is not created by ConcreteFactory"
 
+carbonara = Сarbonara()
+carbonara.add_sauce('egg yolks', '4 pieces')
+carbonara.add_filling('Spaghetti', '400 g')
+carbonara.add_supplements('Salt', 'taste')
+
+bolognese = Bolognese()
+bolognese.add_sauce('Tomatoes in their own juice', '500 g')
+bolognese.add_filling('Spaghetti', '400 g')
+bolognese.add_supplements('Salt', 'taste')
+
+fettuccine_alfredo = Fettuccine_Alfredo()
+fettuccine_alfredo.add_sauce('Cream', '100 ml')
+fettuccine_alfredo.add_filling('Spaghetti', '400 g')
+fettuccine_alfredo.add_supplements('Salt', 'taste')
 
 test_concrete_factory_creation()
 test_product_creation_by_factory()
-print(Сarbonara)
+
