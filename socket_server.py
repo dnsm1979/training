f3cc3cce5639522927a7fb361e10bc6de3975e07
dnsm1979 <dnsm@rambler.ru>
2023-11-28 22:24:37 +0500

    Worcking with DZ_30

--- socket_server.py
+++ socket_server.py
@@ -1,29 +1,31 @@
 import socket
 
+
 def start_server():
-    host = '127.0.0.1'
+    host = "127.0.0.1"
     port = 12345
 
     server_socket = socket.socket()
     server_socket.bind((host, port))
 
     server_socket.listen(1)
-    print(f'Сервер ожидает на {host}:{port}')
+    print(f"Сервер ожидает на {host}:{port}")
 
     while True:
         conn, address = server_socket.accept()
-        print(f'Соединение установлено с {address}')
+        print(f"Соединение установлено с {address}")
 
         while True:
             data = conn.recv(1024).decode()
             if not data:
                 break
-            print(f'Клиент {data=}')
-            message = input('-> ')
+            print(f"Клиент {data=}")
+            message = input("-> ")
             conn.send(message.encode())
 
         conn.close()
-        print('Соединение закрыто')
+        print("Соединение закрыто")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     start_server()
