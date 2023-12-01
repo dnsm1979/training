04ced40e4cc23e2a9bbd37a63f89049e94176a4f
dnsm1979 <dnsm@rambler.ru>
2023-12-01 16:05:13 +0500

    testing and checking dz_30

--- DZ_30_4.py
+++ DZ_30_4.py
@@ -1,30 +1,34 @@
 from threading import Thread
-import shutil, fnmatch, os
+import shutil, fnmatch, os, fileinput, pathlib
 
 class AllThread:
     def __init__(self):
         self.target_path = input('Введите путь к исходному каталогу: ')
         self.target_world =input('Введите слово для поиска: ')
 
+
+
     def search_file(self):
         word = self.target_world
         for root, dirs, files in os.walk(self.target_path):
             for file in files:
-                if file in fnmatch.filter(files, f"*{word}*"):
-                    with open((os.path.join(root,file)),'a', encoding="utf-8") as text:
-                        # list_text = text.read()
-                        with open('finish_text.txt', 'wa', encoding="utf-8") as text_2:
-                            text_2.write(f'{text}\n')
+                if file in fnmatch.filter(files, f"*"+word+"*"):
+                    with open(os.path.join(root,file),'a') as file_read, open('finish_text.txt', 'w') as file_write:
+                        for line in file_read:
+                            if file_read.isfirstline():
+                                file_name = file_read.filename()
+                                file_write.write(f'\n\n------------ {file_name}\n\n')
+                            file_write.write(line)
 
     def search_words(self):
-        words = []
-        with open('finish_text.txt', 'a', encoding="utf-8") as text:
+        words = ['is', 'of']
+        with open('finish_text.txt', 'a') as file_read, open('finish_text.txt', 'w') as file_write:
             for w in words:
-                for line in text:
+                for line in file_read:
                     if w in line:
                         line.replace(w, "")
-        with open('finish_text.txt', 'w', encoding="utf-8") as text2:
-            text2.write(text)
+                        file_write.write(line)
+
 
 
 
@@ -33,11 +37,11 @@ if __name__ == "__main__":
     thread_1 = Thread(target=thread.search_file())
     thread_2 = Thread(target=thread.search_words())
 
-    print('Поток стартует!')
+    print('Поток 1 стартует!')
     thread_1.start()
-    print('Поток завершил работу!')
+    print('Поток 1 завершил работу!')
     thread_1.join()
-    print('Поток стартует!')
+    print('Поток 2 стартует!')
     thread_2.start()
-    print('Поток завершил работу!')
+    print('Поток 2 завершил работу!')
     thread_2.join()
