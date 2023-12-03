ffb2fb334146c97c9b36edd103c5adff22c0e7bb
dnsm1979 <dnsm@rambler.ru>
2023-12-03 23:34:47 +0500

    Working task 3 DZ_31

--- MyProjectDjango/hello_world/hello_world/urls.py
+++ MyProjectDjango/hello_world/hello_world/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from multiplication_table import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.index),
 ]
--- MyProjectDjango/hello_world/multiplication_table/apps.py
+++ MyProjectDjango/hello_world/multiplication_table/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MultiplicationTableConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "multiplication_table"
+
