5ccf88f63adc37167789facdc421672edfa521f7
user <user@mail.ru>
2023-12-04 19:45:14 +0300

    update

---
+++ pythonProject/main.py
@@ -0,0 +1,33 @@
+# file = open('text.txt', 'r')
+# text = file.readlines(2)
+# print(text)
+#
+# file.close()
+
+# with open('text.txt', 'r') as file:
+#     file.seek(5)
+#     print(file.tell())
+#     text = file.read()
+#     print(text)
+
+# with open('text.txt', 'r') as file:
+#     # file.writelines(['Bye\n', 'Bye\n'])
+#     for line in file.readlines():
+#         print(line.strip() + '$')
+
+# import os
+#
+# os.rename('text.txt', 'text2.txt')
+
+# import json
+#
+# human = {'name': 'Иван', 'surname':'Иванов', 'sity':'Москва'}
+#
+# with open('human.json', 'и') as file:
+#     file.write(json.dumps(human))
+# with open('human.json', 'r') as file:
+#     info = json.loads(file.read())
+#     print(info)
+
+
+
