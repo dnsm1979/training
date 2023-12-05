52d5657fc2f262cf734b824b8a48e51f6bac5ddf
dnsm1979 <dnsm@rambler.ru>
2023-12-06 00:09:19 +0500

    worcking with DZ_32

--- MyProjectDjango/hello_world/hello/views.py
+++ MyProjectDjango/hello_world/hello/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from datetime import datetime
+
+
 def index(request):
     current_datetime = datetime.now()
     return HttpResponse(current_datetime)
--- MyProjectDjango/hello_world/hello_world/urls.py
+++ MyProjectDjango/hello_world/hello_world/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from multiplication_table import views
 
 urlpatterns = [
-    path('', views.index),
+    path("", views.index),
 ]
