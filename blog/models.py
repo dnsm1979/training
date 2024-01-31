cda4e22f96267f7baf9c3fd51c9b6923c6fb0005
dnsm1979 <dnsm@rambler.ru>
2024-01-31 22:22:17 +0500

    Start working on blog_progect

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField("Заголовок", max_length=250)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE,)
+    body = models.TextField("Статья")
+    date = models.DateTimeField("Дата публикации")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
