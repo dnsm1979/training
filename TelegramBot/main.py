787b6a9a11f89a68d79d471e7c33ead6d8599ce5
dnsm1979 <dnsm@rambler.ru>
2024-02-12 22:22:34 +0500

    Telegram bot

---
+++ TelegramBot/main.py
@@ -0,0 +1,48 @@
+from telebot import types
+
+import telebot
+
+dict_bot = {'привет': 'Привет!', 'пока': 'До свидания!', 'как дела?': 'Прекрасно!', 'расскажи о себе': 'Я бот!', 'кто ты?': 'я суперБОТ!',
+            'ок': 'OK', 'yes': 'ДА!', 'o': 'O', 'hi': 'Hi', 'H': 'i am fine!'}
+
+bot = telebot.TeleBot('6636364771:AAH0UK8wIMPfLsYIxGEgWjtHd1S0sUZrKGI')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = (types.KeyboardButton('Выставить лот на продажу'))
+    markup.row(btn1)
+    btn2 = (types.KeyboardButton('Посмотреть список вещей на продаже'))
+    markup.row(btn2)
+    bot.send_message(message.chat.id, 'Добро пожаловать!', reply_markup=markup)
+
+@bot.message_handler(commands=['help'])
+def main(message):
+    bot.send_message(message.chat.id, 'По всем вопросом обращайтесь в службу поддержки!', parse_mode='html')
+
+
+
+@bot.message_handler()
+def privet(message):
+
+    x = message.text.lower()
+    if x in dict_bot:
+        bot.send_message(message.chat.id, dict_bot[x])
+
+@bot.message_handler(content_types=['photo'])
+def photos(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = (types.InlineKeyboardButton('Перейти к заполнению формы', url='https://google.com'))
+    btn2 = (types.InlineKeyboardButton('Отменить проверку', callback_data='cancel_checkin'))
+    markup.row(btn1, btn2)
+    bot.reply_to(message, 'Фото отправлено на проверку!', reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_message(callback):
+    if callback.data == 'cancel_checkin':
+        bot.delete_message(callback.message.chat.id, callback.message.message_id -1)
+
+
+
+
+bot.polling(none_stop=True)
