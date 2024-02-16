9f67dd0d99652f00ac583595f5954a93efa5e21f
dnsm1979 <dnsm@rambler.ru>
2024-02-16 23:32:01 +0500

    Working for dz3 Random.org API

--- TelegramBot/dz_randomize3.py
+++ TelegramBot/dz_randomize3.py
@@ -2,12 +2,14 @@ import telebot
 from telebot import types
 from http.client import HTTPSConnection
 from json import dumps, loads
+import time
 
 from telebot.types import InlineKeyboardMarkup
 
 bot = telebot.TeleBot("6636364771:AAH0UK8wIMPfLsYIxGEgWjtHd1S0sUZrKGI")
 API_TOKEN = 'cadbf00e-3f47-474b-9da9-bbf17a4df82a'
 
+ls_score = []
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Привет! ')
@@ -28,24 +30,39 @@ def callback(call):
     connection.request('GET', '/json-rpc/1/invoke', encoded_data, headers)
     response = connection.getresponse()
     response_data = loads(response.read().decode())
-    result1 = response_data["result"]["random"]["data"][0]
-    result2 = response_data["result"]["random"]["data"][1]
-    result3 = response_data["result"]["random"]["data"][2]
-    result4 = response_data["result"]["random"]["data"][3]
-    if result1 + result2 == result3 + result4:
-        bot.send_message(call.message.chat.id, f'{result1}+{result2} против {result3}+{result4}: Розыгрыш!')
-    elif result1 + result2 > result3 + result4:
-        bot.send_message(call.message.chat.id, f'{result1}+{result2} против {result3}+{result4}: Вы проиграли!')
+    ls_score.append(response_data["result"]["random"]["data"][0])
+    ls_score.append(response_data["result"]["random"]["data"][1])
+    ls_score.append(response_data["result"]["random"]["data"][2])
+    ls_score.append(response_data["result"]["random"]["data"][3])
+    bot.send_message(call.message.chat.id, f'Ваш результат: {ls_score[0]}+{ls_score[1]}')
+    time.sleep(3)
+    bot.send_message(call.message.chat.id, 'Теперь кидаю я!')
+    time.sleep(3)
+    bot.send_message(call.message.chat.id, f'Мой результат: {ls_score[2]}+{ls_score[3]}')
+    # result1 = response_data["result"]["random"]["data"][0]
+    # result2 = response_data["result"]["random"]["data"][1]
+    # result3 = response_data["result"]["random"]["data"][2]
+    # result4 = response_data["result"]["random"]["data"][3]
+    if ls_score[0] + ls_score[1] == ls_score[2] + ls_score[3]:
+        bot.send_message(call.message.chat.id, f'{ls_score[0]}+{ls_score[1]} против {ls_score[2]}+{ls_score[3]}: Розыгрыш!')
+    elif ls_score[0] + ls_score[1] < ls_score[2] + ls_score[3]:
+        bot.send_message(call.message.chat.id, f'{ls_score[0]}+{ls_score[1]} против {ls_score[2]}+{ls_score[3]}: Вы проиграли!')
     else:
-        bot.send_message(call.message.chat.id, f'{result1}+{result2} против {result3}+{result4}: Вы выйграли!')
+        bot.send_message(call.message.chat.id, f'{ls_score[0]}+{ls_score[1]} против {ls_score[2]}+{ls_score[3]}: Вы выйграли!')
+    ls_score.clear()
+    markup = types.InlineKeyboardMarkup()
+    button1 = types.InlineKeyboardButton("Играем!", callback_data="start")
+    button2 = types.InlineKeyboardButton("Отмена!", callback_data="stop")
+    markup.add(button1, button2)
+    bot.send_message(call.message.chat.id, "Сыграем ещё?", reply_markup=markup)
 
-    # markup = types.InlineKeyboardMarkup()
-    # button1 = types.InlineKeyboardButton("Играем!", callback_data=start)
-    # button2 = types.InlineKeyboardButton("Отмена!", url='https://habr.com/ru/all/')
-    # markup.add(button1, button2)
-    # bot.send_message(message.chat.id,
-    #                  "{0.first_name}! Сыграем ещё?".format(message.from_user),
-    #                  reply_markup=markup)
+@bot.callback_query_handler(func=lambda callback: True)
+def callback(call):
+    if callback.data == "stop":
+        bot.send_message(call.message.chat.id, f'До свидания!')
+
+    elif callback.data == "start":
+        bot.send_message(call.message.chat.id, f'Отлично!')
 
 
 
