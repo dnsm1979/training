179b2c05a652f06d7743074055b18bb32dd0846c
dnsm1979 <dnsm@rambler.ru>
2024-02-18 18:02:28 +0500

    finish for all dz Random.org API

--- TelegramBot/dz_randomize2.py
+++ TelegramBot/dz_randomize2.py
@@ -3,32 +3,31 @@ from telebot import types
 from http.client import HTTPSConnection
 from json import dumps, loads
 
-from telebot.types import InlineKeyboardMarkup
 
 bot = telebot.TeleBot("6636364771:AAH0UK8wIMPfLsYIxGEgWjtHd1S0sUZrKGI")
 API_TOKEN = 'cadbf00e-3f47-474b-9da9-bbf17a4df82a'
 
-user_ls = []
+
 result_ls = ['Орел', 'Решка']
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Привет! Выберете пожалуйста орел или решку!')
     markup = types.InlineKeyboardMarkup()
-    button1 = types.InlineKeyboardButton("Орел!", callback_data="heads")
-    button2 = types.InlineKeyboardButton("Решка!", callback_data="tails")
+    button1 = types.InlineKeyboardButton(text="Орел!", callback_data="heads")
+    button2 = types.InlineKeyboardButton(text="Решка!", callback_data="tails")
     markup.add(button1, button2)
     bot.send_message(message.chat.id, "Сделайте ваш выбор", reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda callback: True)
+@bot.callback_query_handler(func=lambda callback_query: True)
 def callback(call):
-    if callback == "heads":
-        user_ls.append(1)
+    if call.data == "heads":
+        user_numbers = 1
 
-    elif callback == "tails":
-        user_ls.append(2)
+    elif call.data == "tails":
+        user_numbers = 2
 
-    user_number = user_ls
+    # bot.send_message(call.message.chat.id, user_numbers)
     request_data = {'jsonrpc': '2.0','method': 'generateIntegers','params': {
         'apiKey': API_TOKEN,'min': 1,'max': 2,'n': 1,},'id': 1,
                     }
@@ -41,13 +40,12 @@ def callback(call):
     response = connection.getresponse()
     response_data = loads(response.read().decode())
     result = response_data["result"]["random"]["data"][0]
-    bot.reply_to(call.message, result)
-    if result == user_ls:
-        bot.reply_to(call.message, f'Выпал {result_ls[result - 1]}, Вы победили!')
+    # bot.send_message(call.message.chat.id, result)
+    if result == user_numbers:
+        bot.send_message(call.message.chat.id, f'Выпал {result_ls[result - 1]}, Вы победили!')
     else:
-        bot.reply_to(call.message, f'Выпал {result_ls[result - 1]}, Вы проиграли!')
+        bot.send_message(call.message.chat.id, f'Выпал {result_ls[result - 1]}, Вы проиграли!')
 
-    user_ls.clear()
 
 
 
