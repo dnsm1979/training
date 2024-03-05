fb9188d71d5eef6c4c128d2f7f10ad22c22d1b6c
dnsm1979 <dnsm@rambler.ru>
2024-03-05 21:55:44 +0500

    Synth

---
+++ BD_ORM/models22.py
@@ -0,0 +1,21 @@
+from peewee import *
+
+db = PostgresqlDatabase('peewee', user='postgres', password='root', host='localhost', port='5433')
+
+class Department(Model):
+    name = CharField()
+
+    class Meta:
+        database = db
+
+class Employee(Model):
+    name = CharField()
+    department = ForeignKeyField(Department, backref='employees')
+    salary = DecimalField(max_digits=10, decimal_places=2)
+    experience = IntegerField()
+    class Meta:
+        database = db
+
+
+if __name__ == '__main__':
+    db.create_tables([Department,Employee])
